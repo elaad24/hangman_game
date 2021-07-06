2571bfb51bc145492239a8cd042f2363229e89c5
unknown <elaad2408@gmail.com>
2021-07-06 14:40:19 +0300

    added pic logic

--- src/components/Pic.jsx
+++ src/components/Pic.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Hangman1 from "../images/Hangman1.png";
+import Hangman2 from "../images/Hangman2.png";
+import Hangman3 from "../images/Hangman3.png";
+import Hangman4 from "../images/Hangman4.png";
+import Hangman5 from "../images/Hangman5.png";
+import Hangman6 from "../images/Hangman6.png";
+import Hangman7 from "../images/Hangman7.png";
 
-const Pic = ({}) => {
-  return <div></div>;
+const Pic = ({ lvl }) => {
+  let activePic = Hangman1;
+  switch (lvl) {
+    case 1:
+      activePic = Hangman2;
+      break;
+    case 2:
+      activePic = Hangman3;
+      break;
+    case 3:
+      activePic = Hangman4;
+      break;
+    case 4:
+      activePic = Hangman5;
+      break;
+    case 5:
+      activePic = Hangman6;
+      break;
+    case 6:
+      activePic = Hangman7;
+      break;
+
+    default:
+      activePic = Hangman1;
+      break;
+  }
+  return (
+    <div>
+      <img src={activePic} alt="" />
+    </div>
+  );
 };
 
 export default Pic;
