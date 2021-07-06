10b9f892668a697ac1ab83a4dbaae824481c732c
unknown <elaad2408@gmail.com>
2021-07-06 14:16:55 +0300

    set ganarel states for the app and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-//import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
-
+import Header from "./components/header";
 function App() {
-  return <div className="">work</div>;
+  // loading - pase when the game initiate and get the woed and
+  // replace pic ans letters
+  // gameOver - when the game not started  and or ended
+  // errors - the "bad " point of the player whe he choose incorrect letter
+  // usedLetters - the letters that the game stated with and the letters theat the user choose - way to folaw the letters the user choose
+  // secretWord - the word that the player looging for
+
+  const [loading, setLoading] = useState(false);
+  const [gameOver, setGameOver] = useState(true);
+  const [errors, setErrors] = useState(0);
+  const [usedLetters, setUsedLetters] = useState([]);
+  const [secretWord, setSecretWord] = useState(null);
+
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <div className="header">
+      <h1 className="text_white">HANGMAN</h1>
+    </div>
+  );
+};
+
+export default header;
